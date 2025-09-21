7eb8238014372b97b07f8edf3e4786cfea2dc8f8
Awesome <awesome_devnet@outlook.com>
2025-09-21 16:53:08 +0900

    update

---
+++ prisma/migrations/20250921075303_db_init/migration.sql
@@ -0,0 +1,33 @@
+-- CreateTable
+CREATE TABLE "public"."users" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "hashedPassword" TEXT NOT NULL,
+    "nickName" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "is_admin" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."master" (
+    "user_id" INTEGER NOT NULL,
+    "granted_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "revoked_at" TIMESTAMP(3),
+
+    CONSTRAINT "master_pkey" PRIMARY KEY ("user_id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "public"."users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_nickName_key" ON "public"."users"("nickName");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "master_user_id_key" ON "public"."master"("user_id");
+
+-- AddForeignKey
+ALTER TABLE "public"."master" ADD CONSTRAINT "master_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
